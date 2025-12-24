ce7a87b3330d2e69e19b57eb0b8597c176b86e12
ltdrobot <ltdrobot@localizethedocs.org>
2025-12-24 06:04:35 +0000

    Deploying to pages from @ localizethedocs/zed-docs-l10n@f0b4dc91a32a992d319d02abe808167b1d19e9f8 🚀

---
+++ ltd-config.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["main",    "Development"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Home",    "https://projects.localizethedocs.org/zed-docs-l10n"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/zed-docs-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/zed-docs-l10n"],
+    ["AtomGit", "https://atomgit.com/localizethedocs/zed-docs-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/zed-docs-l10n"],
+  ]
+};
